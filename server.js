c1560aa08638422456b12d4a9a3d048c8b364909
AndreuPuchades <puchadesandreupascual@gmail.com>
2024-10-02 01:46:15 +0200

    Delete comments and add translations

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/save-form', (req, res) => {
+    const { name, email, message } = req.body;
+    const formData = `Nombre: ${name}\nCorreo Electrónico: ${email}\nMensaje: ${message}\n\n`;
+
+    fs.appendFile('formulario.txt', formData, (err) => {
+        if (err) {
+            return res.status(500).send('Error al guardar los datos');
+        }
+        res.send('Datos guardados correctamente');
+    });
+});
